b1b9059797c61ee64063e5e75ddb804d85ac639d
ilke-home <ilke.ertem@amerikadaniste.com>
2017-03-12 21:38:01 +0300

    Homepage revision

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,45 +5,72 @@ angular.module('dikeycozumler',[
         $locationProvider.html5Mode(true).hashPrefix('!');
 
   		$routeProvider.when('/', {
-			templateUrl: 'views/anasayfa.html'
+			templateUrl: 'views/anasayfa.html?v20170312'
 		}).when('/hakkimizda', {
-			templateUrl: 'views/hakkimizda.html'
+			templateUrl: 'views/hakkimizda.html?v20170312'
 		}).when('/hizmetler', {
-			templateUrl: 'views/hizmetler.html'
+			templateUrl: 'views/hizmetler.html?v20170312'
 		}).when('/hizmetler/egitim', {
-			templateUrl: 'views/egitim.html'
+			templateUrl: 'views/egitim.html?v20170312'
 		}).when('/hizmetler/danismanlik', {
-			templateUrl: 'views/danismanlik.html'
+			templateUrl: 'views/danismanlik.html?v20170312'
 		}).when('/hizmetler/montaj', {
-			templateUrl: 'views/montaj.html'
+			templateUrl: 'views/montaj.html?v20170312'
 		}).when('/hizmetler/endustriyel-temizlik', {
-			templateUrl: 'views/endustriyel-temizlik.html'
+			templateUrl: 'views/endustriyel-temizlik.html?v20170312'
 		}).when('/hizmetler/yasam-hatti', {
-			templateUrl: 'views/yasam-hatti.html'
+			templateUrl: 'views/yasam-hatti.html?v20170312'
 		}).when('/hizmetler/ruzgar-enerjisi', {
-			templateUrl: 'views/ruzgar-enerjisi.html'
+			templateUrl: 'views/ruzgar-enerjisi.html?v20170312'
 		}).when('/iletisim', {
-			templateUrl: 'views/iletisim.html'
+			templateUrl: 'views/iletisim.html?v20170312'
 		}).when('/404', {
-			templateUrl: 'views/404.html'
+			templateUrl: 'views/404.html?v20170312'
 		}).otherwise({redirectTo: '/404'});
 	}])
 	.controller('bodyController',[
 		'$scope',
-		function($scope){
+		'$location',
+		function($scope,$location){
 			console.log('bodyController');
 			$scope.slide='slide1';
+
+			$scope.openPage=function(url){
+				$location.url(url);
+			}
 
 			$scope.changeSlide=function(act){
 				console.log(act);
 				switch(act){
 					case 'next':
-						if($scope.slide=='slide1') $scope.slide='slide2'; $scope.slideTextPosition='-100%';
-						if($scope.slide=='slide2') $scope.slide='slide3'; $scope.slideTextPosition='-200%';
+						if($scope.slide=='slide1'){
+							$scope.slide='slide2'; 
+							$scope.slideTextPosition='-95%';
+						}else if($scope.slide=='slide2'){
+							$scope.slide='slide3'; 
+							$scope.slideTextPosition='-190%';
+						}else if($scope.slide=='slide3'){
+							$scope.slide='slide4'; 
+							$scope.slideTextPosition='-285%';
+						}else if($scope.slide=='slide4'){
+							$scope.slide='slide5'; 
+							$scope.slideTextPosition='-380%';
+						}
 						break;
 					case 'prev':
-						if($scope.slide=='slide2') $scope.slide='slide1'; $scope.slideTextPosition='0px';
-						if($scope.slide=='slide3') $scope.slide='slide2'; $scope.slideTextPosition='-100%';
+						if($scope.slide=='slide2'){
+							$scope.slide='slide1'; 
+							$scope.slideTextPosition='0px';
+						}else if($scope.slide=='slide3'){
+							$scope.slide='slide2'; 
+							$scope.slideTextPosition='-95%';
+						}else if($scope.slide=='slide4'){
+							$scope.slide='slide3'; 
+							$scope.slideTextPosition='-190%';
+						}else if($scope.slide=='slide5'){
+							$scope.slide='slide4'; 
+							$scope.slideTextPosition='-285%';
+						}
 						break;
 					default:
 						$scope.slide=act;
